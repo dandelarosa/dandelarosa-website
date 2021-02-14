e5064f9856630ff86726a980c97b6ba1cbf3e1fb
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2021-02-14 17:46:34 -0500

    Use ng-boostrap

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/index', pathMatch: 'full' },
-  { path: 'index', component: IndexComponent },
+  { path: '', component: IndexComponent },
 ];
 
 @NgModule({
